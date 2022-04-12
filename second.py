aab4bc0babbf776e04e33daf59c109dd7e1a0b0b
vinodh1988 <k.c.vinodh@hotmail.com>
2022-04-12 12:43:48 +0530

    if-else

---
+++ second.py
@@ -0,0 +1,16 @@
+a=20
+
+if a>20:
+    print ("a is greater than 20")
+    print (" a seems to be greater than 10 also")
+else:
+    print("less than 20")
+    
+    #python does not have switch statemet
+
+if len("Rahul")>20:
+    print("String is more than 20 characters long")
+elif len("Rahul")>10:
+    print("String is more than 10 characters long")
+else:
+    print("String is less than 10 characters long") 
