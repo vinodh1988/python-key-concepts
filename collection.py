f2414d8b74f49fb1800b62a1233608b364c3b22e
vinodh1988 <k.c.vinodh@hotmail.com>
2022-04-13 09:38:05 +0530

    list-demo

--- collection.py
+++ collection.py
@@ -9,3 +9,18 @@ print(mylist[1:6:2])
 print(mylist[1::2])
 print(mylist[9::-1])
 print(mylist[9:5:-1])
+
+mylist.sort()
+print(mylist)
+mylist.sort(reverse=True)
+print(mylist)
+
+#list is mutable
+mylist.insert(3,15050)
+print(mylist)
+
+mylist.remove(43)
+print(mylist)
+
+mylist.extend([43,35,64])
+print(mylist)
