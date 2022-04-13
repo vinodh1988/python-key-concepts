3566480f0953170e295fde5a05fc10f7d21f3286
vinodh1988 <k.c.vinodh@hotmail.com>
2022-04-13 09:26:31 +0530

    list-slicing

---
+++ collection.py
@@ -0,0 +1,11 @@
+mylist=[1,235,34,54,356,364,43,43,356]
+
+print(mylist)
+print(mylist[1:3])
+print(mylist[0:3])
+print(mylist[3:])
+print(mylist[::])
+print(mylist[1:6:2])
+print(mylist[1::2])
+print(mylist[9::-1])
+print(mylist[9:5:-1])
