eed2a7b01a7a22476ded4197db82caf2a9ff56e1
vinodh1988 <k.c.vinodh@hotmail.com>
2022-04-14 10:03:10 +0530

    nonlocal

--- day-two/closures.py
+++ day-two/closures.py
@@ -3,12 +3,12 @@ def outer(state):
     print("element in outer ",element)
     def inner(data):
         inside = data
+        nonlocal element
+        element= "Test String"
         print("element in inner", element)
         print("inside in inner", inside)
     inner("first Call")
-
+    print("element in outer ",element)
     return inner
         
 ref=outer("Strong")
-ref("India")
-ref("Japan")
