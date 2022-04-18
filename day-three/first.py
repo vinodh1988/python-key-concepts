ca913bfdd24e745fc52e710f0e38759e84834361
vinodh1988 <k.c.vinodh@hotmail.com>
2022-04-18 13:48:24 +0530

    constructor-call

---
+++ day-three/first.py
@@ -0,0 +1,22 @@
+class  Parent:
+    def __init__(self,name="NoName"):
+        self.name =  name 
+    
+    def show(self):
+        print(self.name," is the name of the father")
+
+
+class  Child(Parent):
+    def __init__(self,fname ,name, toyname):
+        self.cname = name
+        super().__init__(fname)
+        self.toyname=toyname
+
+    def print(self):
+        print(self.cname," is the name of the child")
+        print(self.toyname," is the toyname of the child")
+
+
+firstchild=Child("Ganesh","Robin","Elephant")
+firstchild.show()
+firstchild.print()
